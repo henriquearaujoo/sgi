5078133b26e9219b2c13755afff59c2d97e16b85
Leonardo Barreto <lapb@icomp.ufam.edu.br>
2020-11-26 09:56:34 -0400

    Melhorias visuais no form de Eventos.
    Signed-off-by: Leonardo Barreto <lapb@icomp.ufam.edu.br>

--- src/main/java/managedbean/EventoController.java
+++ src/main/java/managedbean/EventoController.java
@@ -4,6 +4,8 @@ import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.faces.application.FacesMessage;
+import javax.faces.context.FacesContext;
 import javax.faces.view.ViewScoped;
 import javax.inject.Inject;
 import javax.inject.Named;
@@ -19,27 +21,26 @@ import util.MakeMenu;
 public class EventoController implements Serializable {
 
 	private static final long serialVersionUID = 1L;
-	
+
 	@Inject
 	private Evento evento;
-	
+
 	private MenuLateral menu = new MenuLateral();
-	
+
 	private List<Evento> eventos = new ArrayList<>();
-	
+
 	private Filtro filtro = new Filtro();
-	
-	public void initListagem(){
+
+	public void initListagem() {
 		carregarLista();
 	}
 
 	@Inject
 	private EventoService eventoService;
-	
-	
-	public EventoController(){}
-	
-	
+
+	public EventoController() {
+	}
+
 	public Filtro getFiltro() {
 		return filtro;
 	}
@@ -47,34 +48,45 @@ public class EventoController implements Serializable {
 	public void setFiltro(Filtro filtro) {
 		this.filtro = filtro;
 	}
-	
-	public String salvar(){
-		eventoService.salvar(evento);	
-		return "eventos?faces-redirect=true";
+
+	public String salvar() {
+		try {
+			eventoService.salvar(evento);
+			return "eventos?faces-redirect=true&sucesso=1";
+		} catch (Exception e) {
+			return "eventos?faces-redirect=true&sucesso=0";
+		}
 	}
-	
-	
-	public void remover(){
-		eventoService.remover(evento);
+
+	public void remover() {
+		try {
+			this.eventoService.remover(this.evento);
+			FacesContext.getCurrentInstance().addMessage("messages",
+					new FacesMessage(FacesMessage.SEVERITY_INFO, "Sucesso", "Evento removido com sucesso!"));
+			this.eventos = this.eventoService.getAll(filtro);
+		} catch (Exception e) {
+			FacesContext.getCurrentInstance().addMessage("messages", new FacesMessage(FacesMessage.SEVERITY_ERROR,
+					"Erro!", "Não foi possível remover o evento desejado."));
+		}
+
+	}
+
+	public void carregarLista() {
 		eventos = eventoService.getAll(filtro);
 	}
 
-	public void carregarLista(){
-		eventos = eventoService.getAll(filtro);
-	}
-	
-	public void carregarEvento(){
+	public void carregarEvento() {
 		evento = eventoService.findById(evento.getId());
 	}
-	
+
 	public List<MenuLateral> getMenus() {
 		return MakeMenu.getMenuFinanceiro();
 	}
-	
+
 	public String redirecionar() {
 		return menu.getEndereco() + "?faces-redirect=true";
 	}
-	
+
 	public Evento getEvento() {
 		return evento;
 	}
@@ -98,6 +110,5 @@ public class EventoController implements Serializable {
 	public void setMenu(MenuLateral menu) {
 		this.menu = menu;
 	}
-
 
 }
