a3bfd70929d5ac4d07fcda3db4647278c8a5ee0b
Caio Francelino <60043104+CaioFrance@users.noreply.github.com>
2021-06-14 22:27:24 -0400

    New help links in finance module

--- src/main/java/managedbean/LancamentoV2Controller.java
+++ src/main/java/managedbean/LancamentoV2Controller.java
@@ -189,6 +189,16 @@ public class LancamentoV2Controller implements Serializable {
 	public List<Projeto> getListaProjeto() {
 		return listaProjeto;
 	}
+	
+	public void dialogHelp() {
+		HashMap<String, Object> options = new HashMap<>();
+		options.put("width", "95%");
+		options.put("height", "95%");
+		options.put("contentWidth", "100%");
+		options.put("contentHeight", "100%");
+		options.put("resizable", false);
+		PrimeFaces.current().dialog().openDynamic("dialog/help/help_lancamentoV2", options, null);
+	}
 
 	public void setListaProjeto(List<Projeto> listaProjeto) {
 		this.listaProjeto = listaProjeto;
