dc326f44b3c4f73bdb94066f54edfb46e4b6fcbd
Caio Francelino <60043104+CaioFrance@users.noreply.github.com>
2021-07-02 17:20:19 -0400

    refactor: add value chart

--- src/main/java/managedbean/OrcamentoController.java
+++ src/main/java/managedbean/OrcamentoController.java
@@ -20,6 +20,9 @@ import javax.inject.Named;
 
 import org.primefaces.PrimeFaces;
 import org.primefaces.component.tabview.TabView;
+import org.primefaces.model.chart.Axis;
+import org.primefaces.model.chart.AxisType;
+import org.primefaces.model.chart.ChartSeries;
 import org.primefaces.model.chart.PieChartModel;
 import org.primefaces.model.charts.bar.BarChartModel;
 import org.primefaces.model.charts.hbar.HorizontalBarChartModel;
@@ -178,6 +181,46 @@ public class OrcamentoController implements Serializable {
 			preCadastro = true;
 
 		PrimeFaces.current().executeScript("setarFocused('info-geral')");
+		
+		chartModelBar();
+	}
+	
+	private org.primefaces.model.chart.BarChartModel model;
+	
+	public org.primefaces.model.chart.BarChartModel getModel() {
+		return model;
+	}
+
+	public void setModel(org.primefaces.model.chart.BarChartModel model) {
+		this.model = model;
+	}
+
+	public void chartModelBar() {
+		model = new org.primefaces.model.chart.BarChartModel();
+        ChartSeries boys = new ChartSeries();
+        boys.setLabel("Boys");
+        boys.set("2004", 120);
+        boys.set("2005", 100);
+        boys.set("2006", 44);
+        boys.set("2007", 150);
+        boys.set("2008", 25);
+        ChartSeries girls = new ChartSeries();
+        girls.setLabel("Girls");
+        girls.set("2004", 52);
+        girls.set("2005", 60);
+        girls.set("2006", 110);
+        girls.set("2007", 135);
+        girls.set("2008", 120);
+        model.addSeries(boys);
+        model.addSeries(girls);
+        model.setTitle("Bar Chart");
+        model.setLegendPosition("ne");
+        Axis xAxis = model.getAxis(AxisType.X);
+        xAxis.setLabel("Gender");
+        Axis yAxis = model.getAxis(AxisType.Y);
+        yAxis.setLabel("Births");
+        yAxis.setMin(0);
+        yAxis.setMax(200);
 	}
 
 	@Inject
